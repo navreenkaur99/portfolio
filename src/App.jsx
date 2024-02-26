5b772d338d5b4e292ae49eac3703108f657b4a38
navreenkaur99 <navreenkaur@gmail.com>
2024-02-26 08:04:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Dashboard from './assets/Pages/Dashboard'
+
+
+export default function App() {
+  return (
+    <Dashboard/>
+  )
+}
---
+++ src/assets/Pages/Dashboard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+export default function Dashboard() {
+  return (
+    <div>
+    <div className="container">
+    <div class="card">
+      <div className='card-body'>
+        <div className='row mt-4'>
+            
+         <div className="col-8">
+          <h1>ABOUT MYSELF</h1>
+          <p>am a person who is positive about every aspect of life. There are many things I like to do, to see, and to experience. I like to read, I like to write; I like to think, I like to dream; I like to talk, I like to listen. </p>
+          <button class="btn btn-primary" type="submit">linkin</button>
+
+        </div>
+            <div className="col-4 "> 
+            
+             <img src="https://www.shutterstock.com/image-photo/young-happy-businesswoman-isolated-on-260nw-2055614918.jpg"/>
+                </div>
+                </div>
+   </div>
+        
+    </div>
+    </div>
+    </div>
+  )
+}
